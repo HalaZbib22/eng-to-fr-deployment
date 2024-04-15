f62121a89d5cb04cd50e355ffb457de230dab5a9
Hala Zbib <hala.zbib@endspacemena.com>
2024-04-15 04:21:40 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import logging
+
+from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
+
+from model.model import EnglishToFrenchTranslator
+
+app = Flask(__name__)  
+CORS(app, support_credentials=True)
+
+# define model, english & french tokenizers paths
+model_path = './model/final_model.h5'
+en_tokenizer_path = './model/english_tokenizer_config.json'
+fr_tokenizer_path = './model/french_tokenizer_config.json'
+
+# create instance
+model = EnglishToFrenchTranslator(model_path, en_tokenizer_path, fr_tokenizer_path)
+logging.basicConfig(level=logging.INFO)
+
+@app.route("/")
+@cross_origin(supports_credentials=True)
+def index():
+    """Render Our Web Page"""
+    return render_template('index.html')
+
+
+@app.route("/translate", methods=["GET"])
+@cross_origin(supports_credentials=True)
+def predict():
+    sentence = request.args.get("english_sentence")
+    prediction = model.predict(sentence)
+    
+    logging.info("prediction from model= {}".format(prediction))
+    return {"french_translation":str(prediction)},200
+
+def main():
+    """Run the Flask app."""
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/model.py
@@ -0,0 +1,51 @@
+import os
+# hide TF warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import tensorflow as tf
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import text
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import numpy as np
+import logging
+import json
+
+class EnglishToFrenchTranslator:
+
+    def __init__(self, model_path, en_tokenizer_path, fr_tokenizer_path):
+        logging.info("EnglishToFrenchTranslator class initialized")
+        self.model = load_model(model_path, compile=False)
+        logging.info("Model is loaded!")
+        logging.info("Loading The English Tokenizer")
+        with open(en_tokenizer_path) as f:
+            data = json.load(f)
+            self.eng_tokenizer = text.tokenizer_from_json(data)
+            logging.info("English Tokenizer is loaded!")
+        logging.info("Loading The French Tokenizer")
+        with open(fr_tokenizer_path) as f:
+            data = json.load(f)
+            self.fr_tokenizer = text.tokenizer_from_json(data)
+            logging.info("French Tokenizer is loaded!")
+        
+
+    def predict(self, sentence):
+        y_id_to_word = {value: key for key, value in self.fr_tokenizer.word_index.items()}
+        y_id_to_word[0] = ''
+
+        sentence = self.eng_tokenizer.texts_to_sequences([sentence])
+        
+        sentence = pad_sequences(sentence, maxlen=15, padding='post')
+        predictions = self.model.predict(sentence)
+
+        return ' '.join([y_id_to_word[np.argmax(x)] for x in predictions[0]])
+
+def main():
+	model = EnglishToFrenchTranslator('final_model.h5', 'english_tokenizer_config.json', 'french_tokenizer_config.json')
+	predicted_class = model.predict("she is driving the truck")
+	logging.info("Translated To French: {}".format(predicted_class)) 
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
